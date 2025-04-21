6244b1f647dded36376ca02ff2203171b4c2014d
Rachit Shah <rachit.sce22@sot.pdpu.ac.in>
2025-04-22 00:31:19 +0530

    updated incomeRoutes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
     let token = req.headers.authorization?.split(" ")[1];
-    // console.log(token);
+
     if (!token) return res.status(401).json({ message: "Not authorized, no token" });
 
     try {
@@ -13,5 +13,5 @@ exports.protect = async (req, res, next) => {
     }
     catch (err) {
         res.status(401).json({ message: "Not authorized, token failed" });
-    }
+    }   
 }
